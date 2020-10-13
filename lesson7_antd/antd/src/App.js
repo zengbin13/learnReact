c4a7f4208f913acf1921d106b27f0ad5f9016c33
zengbin13 <1287538055@qq.com>
2020-10-13 17:30:37 +0800

    antd

---
+++ lesson7_antd/antd/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.less';
+
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button type='primary'>antd</Button>
+    </div>
+  );
+}
+
+export default App;
